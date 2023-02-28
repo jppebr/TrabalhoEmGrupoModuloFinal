49b65b11ddb7e5be675be0ead0616781668c4aa2
jppebr <joaopedro1415@gmail.com>
2023-02-28 15:23:43 -0300

    Update seed.js

--- Backend/scripts/seed.js
+++ Backend/scripts/seed.js
@@ -17,10 +17,12 @@ const seed = async () => {
     const pages = [page]
 
     const products = []
-    for (let i=1; i<=10; i++) {
+    const database = [{ "id": 1, "titulo": "Notícias em tempo real", "descricao": "Acesso a notícias de jogos em tempo real" }, { "id": 2, "titulo": "Busca avançada de notícias", "descricao": "Busca por notícias específicas por categoria, plataforma, editora, desenvolvedor, etc." }, { "id": 3, "titulo": "Filtragem de notícias", "descricao": "Filtragem de notícias por popularidade ou classificação do usuário" }, { "id": 4, "titulo": "Comentários e classificações", "descricao": "Comentários e classificações de usuários" }, { "id": 5, "titulo": "Análises detalhadas de jogos", "descricao": "Acesso a análises de jogos detalhadas" }, { "id": 6, "titulo": "Integração com outras plataformas", "descricao": "Possibilidade de integração com outras plataformas ou aplicativos" }, { "id": 7, "titulo": "Notificações personalizadas", "descricao": "Notificações personalizadas para usuários interessados em determinados jogos" }, { "id": 8, "titulo": "Vídeos e transmissões ao vivo", "descricao": "Vídeos e transmissões ao vivo de jogos" }, { "id": 9, "titulo": "Informações sobre lançamentos e eventos", "descricao": "Informações sobre lançamentos de jogos e eventos relacionados a jogos" }, { "id": 10, "titulo": "Dados estatísticos de jogos", "descricao": "Dados estatísticos de vendas e jogabilidade de jogos populares" }]
+
+    for (let i = 0; i < database.length; i++) {
         const prod = new Product()
-        prod.title = `Produto ${i}`
-        prod.description = `Descrição do produto ${i}`
+        prod.title = database[i].titulo
+        prod.description = database[i].descricao
         products.push(prod)
     }
 
@@ -28,7 +30,7 @@ const seed = async () => {
     admin.email = "admin@case2.com"
     admin.encryptedPassword = hashSync('12345678', 10)
     const users = [admin]
-    
+
     await Page._seed(pages)
     await Product._seed(products)
     await User._seed(users)
